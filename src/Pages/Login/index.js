a6c05170852f5f6235beb24b380a011994e6c13e
Cassio Fernandes <cassio.rfernandes@gmail.com>
2024-09-17 07:12:22 -0300

    Incluindo input aplicacao

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Form, Container } from "./styles";
+import Input from "../../Components/Input/index";
 
 const Login = () => {
     return ( 
         <Container>
             <Form>
              <h1> Faça seu login!👋</h1>
+             <Input/>
+             <Input/>
              <div>
                 <p>Não possuí conta?</p>
                 <a href="#/">Cadastrar</a>
